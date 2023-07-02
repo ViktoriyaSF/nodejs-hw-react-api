3fcfabc0679f4c83dd1cb4ec293408bd9401768f
ViktoriyaSF <kievstar01051985@gmail.com>
2023-07-02 18:36:22 +0300

    app.use(404)

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.use("/api/contacts", contactsRouter);
 
 app.use(globalErrorHandler);
 
-// app.use((req, res) => {
-//   res.status(404).json({ message: "Not found" });
-// });
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
 
 // app.use((err, req, res, next) => {
 //   res.status(500).json({ message: err.message });
