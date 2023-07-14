5a91173baf307b20d4b3c3ef491a1ea79ea1b5a5
ViktoriyaSF <kievstar01051985@gmail.com>
2023-07-14 22:43:41 +0300

    create new route

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require("mongoose");
+
+const { handleSchemaValidationErrors } = require("../utils");
+
+const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+const subscriptionList = ["starter", "pro", "business"];
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      minlength: 6,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegexp,
+    },
+    subscription: {
+      type: String,
+      enum: subscriptionList,
+      default: "starter",
+    },
+    token: String,
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  { versionKey: false }
+);
+
+userSchema.post("save", handleSchemaValidationErrors);
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  subscriptionList,
+  emailRegexp,
+};
