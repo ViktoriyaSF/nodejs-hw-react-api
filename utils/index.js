3fff86c12e405abeb4cbf02c12a5ccc720b44052
ViktoriyaSF <kievstar01051985@gmail.com>
2023-07-23 22:34:58 +0300

    nodemailer package is installed and created sendEmail

--- utils/index.js
+++ utils/index.js
@@ -2,10 +2,12 @@ const { HttpError } = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleSchemaValidationErrors = require("./handleSchemaValidationErrors");
 const newAvatarSize = require("./newAvatarSize");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
   HttpError,
   ctrlWrapper,
   handleSchemaValidationErrors,
   newAvatarSize,
+  sendEmail,
 };
---
+++ utils/sendEmail.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+
+const { UKR_NET_EMAIL, UKR_NET_PASSWORD } = process.env;
+
+const nodemailerConfig = {
+  host: "smtp.meta.ua",
+  port: 465,
+  secure: true,
+  auth: {
+    user: UKR_NET_EMAIL,
+    pass: UKR_NET_PASSWORD,
+  },
+};
+const transporter = nodemailer.createTransport(nodemailerConfig);
+
+const sendEmail = async (data) => {
+  try {
+    const email = { ...data, from: UKR_NET_EMAIL };
+    await transporter.sendMail(email);
+    return true;
+  } catch (error) {
+    console.log("An error occurred while sending mail");
+  }
+};
+
+module.exports = sendEmail;
